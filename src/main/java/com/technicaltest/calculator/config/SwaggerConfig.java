887d5cc1658fc233153f7734cb3216c48cfd69c5
Mike <miguelcompanysoler@gmail.com>
2020-11-27 15:06:24 +0100

    Swagger UI config created

---
+++ src/main/java/com/technicaltest/calculator/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.technicaltest.calculator.config;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket tasksApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+                .apiInfo(apiInfo()).select().paths(taskPaths()).build();
+    }
+
+    private Predicate<String> taskPaths() {
+        return or(regex("/api/v1/calculator"),
+                regex("/api/v1/calculator/.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Calculator API")
+                .description("Calculator API in Java").version("1.0").build();
+    }
+}
